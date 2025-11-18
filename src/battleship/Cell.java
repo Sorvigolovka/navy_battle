1e1ee6f4e2d00caba12bbfa2455d99f77565bbf4
Illia-Norov <illia.norov@gmail.com>
2025-11-18 19:00:29 +0200

    Add Swing main menu and localization

---
+++ src/battleship/Cell.java
@@ -0,0 +1,41 @@
+package battleship;
+
+class Cell {
+    private final int row;
+    private final int col;
+    private Ship ship;
+    private boolean shot;
+
+    Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    int getRow() {
+        return row;
+    }
+
+    int getCol() {
+        return col;
+    }
+
+    Ship getShip() {
+        return ship;
+    }
+
+    void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    boolean hasShip() {
+        return ship != null;
+    }
+
+    boolean isShot() {
+        return shot;
+    }
+
+    void markShot() {
+        this.shot = true;
+    }
+}
