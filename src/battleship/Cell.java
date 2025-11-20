79f2fcc11646fab446d2c4f94994f81fe2a05bc1
Illia-Norov <illia.norov@gmail.com>
2025-11-20 18:45:08 +0200

    Handle online game conclusion messaging and reset

---
+++ src/battleship/Cell.java
@@ -0,0 +1,81 @@
+package battleship;
+
+import java.io.Serializable;
+
+class Cell implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final int row;
+    private final int col;
+    private Ship ship;
+    private boolean shot;
+    private boolean miss;
+    private boolean remoteHit;
+    private boolean remoteSunk;
+
+    Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    int getRow() {
+        return row;
+    }
+
+    int getCol() {
+        return col;
+    }
+
+    Ship getShip() {
+        return ship;
+    }
+
+    void setShip(Ship ship) {
+        this.ship = ship;
+    }
+
+    boolean hasShip() {
+        return ship != null;
+    }
+
+    boolean isShot() {
+        return shot;
+    }
+
+    void markShot() {
+        this.shot = true;
+    }
+
+    void setShot(boolean shot) {
+        this.shot = shot;
+    }
+
+    boolean isMiss() {
+        return miss;
+    }
+
+    void setMiss(boolean miss) {
+        this.miss = miss;
+    }
+
+    boolean isRemoteHit() {
+        return remoteHit;
+    }
+
+    void setRemoteHit(boolean remoteHit) {
+        this.remoteHit = remoteHit;
+        if (!remoteHit) {
+            this.remoteSunk = false;
+        }
+    }
+
+    boolean isRemoteSunk() {
+        return remoteSunk;
+    }
+
+    void setRemoteSunk(boolean remoteSunk) {
+        this.remoteSunk = remoteSunk;
+        if (remoteSunk) {
+            this.remoteHit = true;
+        }
+    }
+}
---
+++ src/battleship/SaveManager.java
@@ -0,0 +1,49 @@
+package battleship;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class SaveManager {
+    private static final String SAVE_DIR = "saves";
+
+    static List<String> listSaves() {
+        File dir = new File(SAVE_DIR);
+        if (!dir.exists()) {
+            return List.of();
+        }
+        String[] names = dir.list((d, name) -> name.endsWith(".sav"));
+        if (names == null) {
+            return List.of();
+        }
+        Arrays.sort(names);
+        List<String> result = new ArrayList<>();
+        for (String n : names) {
+            result.add(n);
+        }
+        return result;
+    }
+
+    static void save(GameState state, String name) throws IOException {
+        Files.createDirectories(Path.of(SAVE_DIR));
+        File file = new File(SAVE_DIR, name.endsWith(".sav") ? name : name + ".sav");
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
+            out.writeObject(state);
+        }
+    }
+
+    static GameState load(String name) throws IOException, ClassNotFoundException {
+        File file = new File(SAVE_DIR, name.endsWith(".sav") ? name : name + ".sav");
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+            return (GameState) in.readObject();
+        }
+    }
+}
